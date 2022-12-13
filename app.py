7bfc0643a050885011460ddc5229b08bd4179460
esty4100 <estyk4100@gmail.com>
2022-12-13 09:01:29 +0200

    my commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from typing import Tuple
-
 from flask import Flask, jsonify, request, Response
+from sympy import primenu
+
 import mockdb.mockdb_interface as db
 
 app = Flask(__name__)
@@ -45,7 +46,29 @@ def create_response(
 def hello_world():
     return create_response({"content": "hello world!"})
 
+@app.route("/users/all")
+def allUsers():
+    return create_response({"data":db.get("users")})
 
+@app.route("/users/<userId>")
+def byId(userId):
+    ans=db.getById("users",int(userId))
+    if ans is None:
+        return create_response(status=404,message="There is no user with such an id")
+    return create_response({"data":ans})
+
+@app.route("/users")
+def byTeam():
+    team=request.args.get("team")
+    users=[i for i in db.get("users") if i["team"] == team]
+    if users==[]:
+        return create_response(message="There is no users in this team!",status=404)
+    return create_response({"users":users})
+
+@app.route("/users",methods=['POST'])
+def insertUser():
+    user=request.json
+    return db.create("users",user)
 @app.route("/mirror/<name>")
 def mirror(name):
     data = {"name": name}
